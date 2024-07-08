fa5d28899afe7423286d84ce6650687cc9f5ea93
TypicalNerd9 <charly-perea@live.com>
2024-07-08 14:08:12 -0600

    Initial commit

---
+++ src/main/java/com/github/typicalnerd9/flixcache/flixcache/FlixcacheApplication.java
@@ -0,0 +1,13 @@
+package com.github.typicalnerd9.flixcache.flixcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixcacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/typicalnerd9/flixcache/flixcache/FlixcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.typicalnerd9.flixcache.flixcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
